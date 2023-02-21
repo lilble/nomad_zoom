9b96d8603895f7de2f27f6abfd2d55903c47b513
lilble <77142800+lilble@users.noreply.github.com>
2023-02-22 04:59:49 +0900

    example

--- src/server.js
+++ src/server.js
@@ -1,20 +1,36 @@
-import http from "http"
+import http from "http";
 import WebSocket from "ws";
 import express from "express";
-
 const app = express();
-
-app.set("view engine", "pug"); // template engine의 종류 : pug
-app.set("views", __dirname + "/views"); // template file 위치
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home")); // route handler - rendering "home.pug"
-app.get("/*", (req, res) => res.redirect("/"));
-
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-// app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+function onSocketClose() {
+  console.log("Disconnected from the Browser ❌");
+}
+const sockets = [];
 
-// 같은 서버에서 http, web socket 둘 다 작동 가능
-const server = http.createServer(app);  
-const wss = new WebSocket.Server({server}); 
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser ✅");
+  socket.on("close", onSocketClose);
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
+});
 
 server.listen(3000, handleListen);
